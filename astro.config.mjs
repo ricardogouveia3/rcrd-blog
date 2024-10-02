4c1b42d1bd02ddb9596e688fd097cf4b0d9a9bbc
Ricardo Gouveia <ricardogouveia3_@hotmail.com>
2024-10-02 08:32:50 -0300

    remove mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://blog.rcrd.dev',
-    integrations: [mdx(), sitemap(), tailwind()],
+    integrations: [sitemap(), tailwind()],
 });
