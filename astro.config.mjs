6ce3739c78d49492b02e3fc582ea0066f3ad60ec
Ricardo Gouveia <ricardogouveia3_@hotmail.com>
2024-10-03 07:18:05 -0300

    ignore markdown style guide, config shiki syntax highlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,16 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://blog.rcrd.dev',
-    integrations: [sitemap(), tailwind()],
+	site: 'https://blog.rcrd.dev',
+	integrations: [sitemap(), tailwind()],
+	markdown: {
+		syntaxHighlight: 'shiki',
+		shikiConfig: {
+			theme: 'dracula-soft',
+			defaultColor: false,
+			langs: [],
+			wrap: true,
+			transformers: [],
+		},
+	},
 });
